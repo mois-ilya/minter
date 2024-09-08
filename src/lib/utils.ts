08e24cebbac951305775960d603c968b1b656cbb
Moiseev Ilya <ilya@mois.pro>
2024-09-08 21:14:17 +0400

    chore: migrated seqno waiter

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { Address, beginCell, toNano, TonClient } from "@ton/ton";
 import { JettonDeployParams, JETTON_DEPLOY_GAS } from "./deploy-controller";
 import { initData, JETTON_MINTER_CODE, mintBody } from "./jetton-minter";
+import { getClient } from "./get-ton-client";
+import { makeGetCall } from "./make-get-call";
 
 export async function sleep(time: number) {
   return new Promise((resolve) => {
@@ -19,14 +21,19 @@ export function zeroAddress(): Address {
     .loadAddress();
 }
 
-export async function waitForSeqno(wallet: any) {
-  // todo: type Wallet
-  const seqnoBefore = await wallet.getSeqNo();
+async function getSeqno(address: Address, tc: TonClient) {
+  return makeGetCall(address, "seqno", null, tc).then((reader) => reader.readBigNumber());
+}
+
+export async function waitForSeqno(address: string) {
+  const tc = await getClient();
+  const ownerAddress = Address.parse(address);
+  const seqnoBefore = await getSeqno(ownerAddress, tc);
 
   return async () => {
     for (let attempt = 0; attempt < 25; attempt++) {
       await sleep(3000);
-      const seqnoAfter = await wallet.getSeqNo();
+      const seqnoAfter = await getSeqno(ownerAddress, tc);
       if (seqnoAfter > seqnoBefore) return;
     }
     throw new Error("Timeout");
